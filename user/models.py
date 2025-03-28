becf2ef3f31f8fdd10d22c50d202748dfe78ebea
14elias <ellamebrahtom1995@gmail.com>
2025-03-28 12:37:22 +0300

    customizing user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,65 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser,PermissionsMixin
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None,**kwargs):
+        """
+        Creates and saves a User with the given email, date of
+        birth and password.
+        """
+        if not email:
+            raise ValueError("Users must have an email address")
+        email=self.normalize_email(email)
+        email=email.lower()
+
+        user = self.model(
+            email=email,
+            **kwargs
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email,  password=None,**kwargs):
+        """
+        Creates and saves a superuser with the given email, date of
+        birth and password.
+        """
+        user = self.create_user(
+            email,
+            password=password,
+            **kwargs
+        )
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(
+        verbose_name="email address",
+        max_length=255,
+        unique=True,
+    )
+    date_of_birth = models.DateField(null=True,blank=True)
+    first_name=models.CharField(max_length=100)
+    last_name=models.CharField(max_length=100)
+
+    is_active = models.BooleanField(default=True)
+
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name","last_name"]
+
+    def __str__(self):
+        return self.email
+
+    
